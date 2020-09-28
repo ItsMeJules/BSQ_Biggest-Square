734229c4c4cff4e3c13adb4f4dbfbc52fc747115
Jules PEYRON <jpeyron@e2r4p6.42.fr>
2020-09-28 19:07:40 +0200

    updated squares methods

--- includes/square.h
+++ includes/square.h
@@ -6,27 +6,21 @@
 /*   By: jpeyron <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 16:44:44 by jpeyron           #+#    #+#             */
-/*   Updated: 2020/09/28 18:18:28 by jpeyron          ###   ########.fr       */
+/*   Updated: 2020/09/28 19:04:19 by jpeyron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SQUARE_H
 # define SQUARE_H
 
-int			square_contains(t_square sq, t_point pt);
-int			square_len(t_square sq, int a);
-t_square	create_square(t_point a, t_point b);
-
-typedef struct	s_point
-{
-	int	x;
-	int	y;
-}				t_point;
+int			square_contains(t_square sq, int x, int y);
+t_square	create_square(int x, int y, int len);
 
 typedef struct	s_square
 {
-	t_point	min;
-	t_point	max;
+	int	minX;
+	int	minY;
+	int	len;
 }				t_square;
 
 #endif
--- srcs/square.c
+++ srcs/square.c
@@ -6,40 +6,32 @@
 /*   By: jpeyron <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 16:48:57 by jpeyron           #+#    #+#             */
-/*   Updated: 2020/09/28 18:20:54 by jpeyron          ###   ########.fr       */
+/*   Updated: 2020/09/28 19:05:37 by jpeyron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "math_utils.h"
 #include "square.h"
 
-int			square_contains(t_square sq, t_point pt)
+int			square_contains(t_square sq, int x, int y)
 {
-	return (sq.max.x >= pt.x && sq.min.x <= pt.x &&
-			sq.max.y >= pt.y && sq.min.y <= pt.y);
+	return ((sq.minX + sq.len) >= x && sq.minX <= x &&
+			(sq.minY + sq.len) >= y && sq.minY <= y);
 }
 
-int			square_len(t_square sq, int a)
+void		expand_square(t_square *sq)
 {
-	if (a)
-		return (sq.max.x - sq.min.x);
-	else
-		return (sq.max.y - sq.min.y);
+	sq->x += 1;
+	sq->y += 1;
 }
 
-t_square	create_square(t_point a, t_point b)
+t_square	create_square(int x, int y, int len)
 {
 	t_square	sq;
 
 	if (!(sq = malloc(sizeof(t_square))))
 		return (NULL);
-	if (!(sq.min = malloc(sizeof(t_point))))
-		return (NULL);
-	if (!(sq.max = malloc(sizeof(t_point))))
-		return (NULL);
-	sq.min.x = min(a.x, b.x);
-	sq.min.y = min(a.y, b.y);
-	sq.max.x = max(a.x, b.x);
-	sq.max.y = max(a.y, b.y);
+	sq.minX = x;
+	sq.minY = y;
+	sq.len = len;
 	return (sq);
 }
