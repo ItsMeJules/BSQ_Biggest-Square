ecd48bb4fb9294b95b8ecb68453c8ed42a4ecfd4
Jules PEYRON <jpeyron@e2r4p6.42.fr>
2020-09-29 19:04:09 +0200

    started resolve alg

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jpeyron <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 20:27:56 by jpeyron           #+#    #+#             */
-/*   Updated: 2020/09/29 18:01:32 by jpeyron          ###   ########.fr       */
+/*   Updated: 2020/09/29 19:03:52 by jpeyron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,25 @@
 
 char	*resolve_map(char *file)
 {
-	t_map	*map;
+	t_map		*map;
+	t_square	sq;
 
 	map = get_map(file);
 	if (map_error(map->tab))
 		return ("map error\n");
-
-
+	sq = create_square(0, 0, 0);
+	while (sq.min_y + sq.len <= map.height + 1)
+	{
+		if (!has_obstacle(sq, map))
+		{
+			while (!has_obstacle_wall(sq, map))
+			{
+				sq.len++;
+			}
+		}
+		else
+			sq.len++;
+	}
 }
 
 int	main(int ac, char **av)
